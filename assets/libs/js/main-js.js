27df9b53bed2aeb20f1a6de324256879c328a37e
Thalita <thalitaribeirooliveira@gmail.com>
2019-05-05 19:36:59 -0400

    Terminei as telas de dashboard e comecei a fazer a tela de registro de refeição.

--- assets/libs/js/main-js.js
+++ assets/libs/js/main-js.js
@@ -1,6 +1,17 @@
-
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
     'use strict';
+
+    // ==============================================================
+    // Altura mínima da janela
+    // ==============================================================
+    var alturaJanela = $(window).height();
+    $('.dashboard-content').css('min-height', alturaJanela - 61);
+    $('.splash-container').css('min-height', alturaJanela - 60);
+
+    $(window).resize(function () {
+        $('.dashboard-content').css('min-height', alturaJanela - 61);
+        $('.splash-container').css('min-height', alturaJanela - 60);
+    });
 
     // ============================================================== 
     // Notification list
@@ -19,9 +30,7 @@ jQuery(document).ready(function($) {
 
 
     if ($(".menu-list").length) {
-        $('.menu-list').slimScroll({
-
-        });
+        $('.menu-list').slimScroll({});
     }
 
     // ============================================================== 
@@ -30,9 +39,9 @@ jQuery(document).ready(function($) {
 
     if ($(".sidebar-nav-fixed a").length) {
         $('.sidebar-nav-fixed a')
-            // Remove links that don't actually link to anything
+        // Remove links that don't actually link to anything
 
-            .click(function(event) {
+            .click(function (event) {
                 // On-page links
                 if (
                     location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
@@ -47,7 +56,7 @@ jQuery(document).ready(function($) {
                         event.preventDefault();
                         $('html, body').animate({
                             scrollTop: target.offset().top - 90
-                        }, 1000, function() {
+                        }, 1000, function () {
                             // Callback after animation
                             // Must change focus!
                             var $target = $(target);
@@ -57,11 +66,13 @@ jQuery(document).ready(function($) {
                             } else {
                                 $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
                                 $target.focus(); // Set focus again
-                            };
+                            }
+                            ;
                         });
                     }
-                };
-                $('.sidebar-nav-fixed a').each(function() {
+                }
+                ;
+                $('.sidebar-nav-fixed a').each(function () {
                     $(this).removeClass('active');
                 })
                 $(this).addClass('active');
@@ -73,60 +84,30 @@ jQuery(document).ready(function($) {
     // tooltip
     // ============================================================== 
     if ($('[data-toggle="tooltip"]').length) {
-            
-            $('[data-toggle="tooltip"]').tooltip()
 
-        }
-
-     // ============================================================== 
-    // popover
-    // ============================================================== 
-       if ($('[data-toggle="popover"]').length) {
-            $('[data-toggle="popover"]').popover()
+        $('[data-toggle="tooltip"]').tooltip()
 
     }
-     // ============================================================== 
+
+    // ==============================================================
+    // popover
+    // ============================================================== 
+    if ($('[data-toggle="popover"]').length) {
+        $('[data-toggle="popover"]').popover()
+
+    }
+    // ==============================================================
     // Chat List Slim Scroll
     // ============================================================== 
-        
 
-        if ($('.chat-list').length) {
-            $('.chat-list').slimScroll({
+
+    if ($('.chat-list').length) {
+        $('.chat-list').slimScroll({
             color: 'false',
             width: '100%'
 
 
         });
     }
-    // ============================================================== 
-    // dropzone script
-    // ============================================================== 
-
- //     if ($('.dz-clickable').length) {
- //            $(".dz-clickable").dropzone({ url: "/file/post" });
- // }
 
 }); // AND OF JQUERY
-
-
-// $(function() {
-//     "use strict";
-
-
-    
-
-   // var monkeyList = new List('test-list', {
-    //    valueNames: ['name']
-
-     // });
-  // var monkeyList = new List('test-list-2', {
-    //    valueNames: ['name']
-
-   // });
-
-
-
-   
-   
-
-// });
